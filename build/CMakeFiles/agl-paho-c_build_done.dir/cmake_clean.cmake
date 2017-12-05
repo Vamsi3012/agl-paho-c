57c6e9d3b98946e1e3cb5fe2c7396d7c5c5e39e1
José Bollo <jose.bollo@iot.bzh>
2017-12-05 10:37:59 +0100

    Remove build artifacts
    Change-Id: I660c73df06cc5c258fa1761914e76a59df7dd877
    Signed-off-by: José Bollo <jose.bollo@iot.bzh>

--- build/CMakeFiles/agl-paho-c_build_done.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "agl-paho-c.wgt"
-  "CMakeFiles/agl-paho-c_build_done"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/agl-paho-c_build_done.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
